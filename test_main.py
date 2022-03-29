c3c8dcc51b8537baa2606c17ba6ac890826299a0
AK7795 <akshaysingodi@gmail.com>
2022-03-29 15:09:49 +0530

    wh2

--- test_main.py
+++ test_main.py
@@ -1,54 +1,27 @@
 import main
 import pytest
 
-@pytest.fixture
-def input():
-    a = 25
-    return a
+@pytest.mark.parametrize("n1,out1",[(5,True),(7,False),(10,True),(9,False)])
+def test_divby5_1(n1,out1):
+    out1 = main.divby5(n1)
+    assert out1 == out1
 
-def test_divby5_1(input):
-    #a = 10
-    out1 = main.divby5(input)
-    assert out1 == True
-
-
-def test_divby5_2(input):
-    #a = 11
-    out1 = main.divby5(input)
-    assert out1 == False
+@pytest.mark.parametrize("n2,out2",[(7,True),(8,False),(14,True),(20,False)])
+def test_divby7_1(n2,out2):
+    out1 = main.divby7(n2)
+    assert out1 == out2
 
 
-def test_divby7_1(input):
-    #a = 14
-    out1 = main.divby7(input)
-    assert out1 == True
+@pytest.mark.parametrize("n3,out3",[(9,True),(8,False),(18,True),(20,False)])
+def test_divby9_1(n3,out3):
+    out1 = main.divby9(n3)
+    assert out1 == out3
 
 
-def test_divby7_2(input):
-    #a = 15
-    out1 = main.divby7(input)
-    assert out1 == False
+@pytest.mark.parametrize("n4,out4",[(11,True),(8,False),(33,True),(20,False)])
+def test_divby11_1(n4,out4):
+    out1 = main.divby11(n4)
+    assert out1 == out4
 
 
-def test_divby9_1(input):
-    #a = 18
-    out1 = main.divby9(input)
-    assert out1 == True
 
-
-def test_divby9_2(input):
-
-    out1 = main.divby9(input)
-    assert out1 == False
-
-
-def test_divby11_1(input):
-
-    out1 = main.divby11(input)
-    assert out1 == True
-
-
-def test_divby11_2(input):
-
-    out1 = main.divby11(input)
-    assert out1 == False
